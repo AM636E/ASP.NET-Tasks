80f3955e6c0d7e0bde519a6d91acb1b75cb6336a
Nazariy <bnazariy@gmail.com>
2014-06-25 21:59:10 +0300

    Last

---
+++ TodoList/Code/Managers/Security/SecurityManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataAccess;
+
+namespace TodoList.Code.Managers.Security
+{
+    public class SecurityManager
+    {
+        private readonly IUserRepository _repository;        
+    }
+}
